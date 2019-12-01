175e8c1f8589b654e9fb895611a4ed40e0b248f4
joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-01 00:23:22 -0500

    ysl: Build libvndfwk_detect_jni.qti
    * Build both core and vendor versions
    
    Change-Id: I527762fbbd0cce0aeee4017af08bcd502cba6044
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -199,7 +199,9 @@ PRODUCT_PACKAGES += \
 # Fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
-    libqti_vndfwk_detect.vendor
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
