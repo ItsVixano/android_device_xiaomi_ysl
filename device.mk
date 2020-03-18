ee0b920251f854fa12eae93cde098260d9443b2b
TheStrix <parthbhatia98@gmail.com>
2020-03-19 02:38:50 +0530

    ysl: Use new QTI components from qcom/common
    Change-Id: Idab3a2cf06813bfa8e38811faeb1266c4d3b7434
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -388,6 +388,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# QTI Common
+TARGET_COMMON_QTI_COMPONENTS := \
+    telephony
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.ysl
