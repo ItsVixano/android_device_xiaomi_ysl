cc75b9c8617e76b83c84659ea878946aa6523808
ix5 <ix5@users.noreply.github.com>
2020-04-29 21:08:37 +0200

    ysl: Add protobuf vendorcompat lib
    Our blobs are compiled against libprotobuf-full-cpp.so, but
    starting in R, Android is using a versioned naming approach,
    e.g. libprotobuf-cpp-full-3.9.1.so.
    
    See https://r.android.com/1109518
    
    The entry in public.libraries.txt is needed to allow the
    linker to find and use the lib outside the VNDK.
    
    See https://source.android.com/devices/tech/config/namespaces_libraries
    
    [wight554: added libprotobuf-cpp-lite as well]
    
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Change-Id: I28222c89e1e07017867088492a726ee9d45d8e6f

--- device.mk
+++ device.mk
@@ -297,6 +297,15 @@ PRODUCT_COPY_FILES += \
 # Properties
 -include device/xiaomi/ysl/prop.mk
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
+
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
