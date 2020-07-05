ea1a2ef795fb0d72d6b17b009f59c0ed331e9a2e
0ranko0P <ranko0p@outlook.com>
2020-07-06 01:54:07 +0800

    ysl: Set zygote preforking in system_prop
    Move it to PRODUCT_SYSTEM_DEFAULT_PROPERTIES to fix violated by
    
    avc:  denied  { set } for property=persist.device_config.runtime_native.usap_pool_enabled pid=1 uid=0 gid=0
    scontext=u:r:vendor_init:s0
    tcontext=u:object_r:device_config_runtime_native_prop:s0 tclass=property_service
    
    Change-Id: Ic1c586e25a15f420db39fadfa03e9e47889a81a9

--- prop.mk
+++ prop.mk
@@ -277,7 +277,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
 
 # Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 persist.device_config.runtime_native.usap_pool_enabled=true
 
 # Boot
