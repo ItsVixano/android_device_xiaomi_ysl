67c949d2f681b969c2053479f1d44af7f0e1baee
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 20:26:31 +0530

    ysl: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -354,6 +354,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/ysl
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
