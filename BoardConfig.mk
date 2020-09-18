d95fed28ec2f3fac0d46815d628a6bb8049677ce
LuK1337 <priv.luk@gmail.com>
2020-09-18 10:57:02 +0200

    ysl: Update qcom sepolicy include
    Change-Id: I7167575e82eb8c5de2d822643261757edda73a07
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-09-05
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
