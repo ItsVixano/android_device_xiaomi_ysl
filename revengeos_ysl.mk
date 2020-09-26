9714e35231bfea8f3c707b67630244d19cd556a1
ghostrider-reborn <adithya.r02@outlook.com>
2020-09-26 09:19:26 +0000

    ysl: Override build description to fp
    Change-Id: I2d7f870a2026c3b07423dd3724cc6d4aa747af8b

--- revengeos_ysl.mk
+++ revengeos_ysl.mk
@@ -39,8 +39,12 @@ TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+# Build fingerprint
+BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
+BUILD_DESC := tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys
 
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
