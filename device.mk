b47d77db6c8ddeab9bc5365acafbb59b737c09be
Giovanni Ricca <50358281+ItsVixano@users.noreply.github.com>
2020-10-22 19:51:16 +0200

    ysl: use prebuilt sensorservice from vndk v27
    * PLS BOOT REEEE
    * HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager
    
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -379,6 +379,8 @@ PRODUCT_PACKAGES += \
     com.android.vndk.current.on_vendor
 
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0.so \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
