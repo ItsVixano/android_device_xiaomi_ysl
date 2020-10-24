1d1c67b08f91a058cdfebdb57615e8f2506f7b58
ItsVixano <vixanonew@gmail.com>
2020-10-24 23:37:03 +0200

    ysl: Bump vendor SPL from LA.UM.9.6.2.r1-02100-89xx.0
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,7 +194,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2020-04-05
+VENDOR_SECURITY_PATCH := 2020-09-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
