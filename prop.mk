f3ec4fd4a2b974255ac1bce78016ed2bdc97f72b
sourajitk <sourajit@live.com>
2020-11-08 14:52:58 +0100

    ysl: Drop all SF offset related props.
    For triple frame buffers 1938659 to work properly, these props
    need to be dropped to prevent any unneeded regressions.
    
    Change-Id: I058cef2c28af1d6512d88f7fc5e840c0aaedbd01
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- prop.mk
+++ prop.mk
@@ -225,16 +225,11 @@ persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974
 
 # SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-ro.surface_flinger.protected_contents=true
-
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=11600000 \
-debug.sf.early_app_phase_offset_ns=11600000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
-debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
