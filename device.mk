0a84061678cdb51abeee19268053eadf22dfade6
ItsVixano <vixanonew@gmail.com>
2021-01-16 10:22:45 +0000

    ysl: use prebuilt sensorservice from vndk v27
    * PLS BOOT REEEE
    * HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager
    
    Signed-off-by: ItsVixano <vixanonew@gmail.com>

--- device.mk
+++ device.mk
@@ -398,6 +398,10 @@ PRODUCT_PACKAGES += \
     vndk_package \
     com.android.vndk.current.on_vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0.so \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0.so
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
