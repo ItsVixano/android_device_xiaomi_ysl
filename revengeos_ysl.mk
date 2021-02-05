984f6ed4746c9e27c74f0e372780dc814587fc0d
ItsVixano <vixanonew@gmail.com>
2021-02-05 07:20:47 +0000

    ysl: ops :/
    Change-Id: I541c43ef2ccce3f6612b1696354853fb0e015237

--- revengeos_ysl.mk
+++ revengeos_ysl.mk
@@ -47,7 +47,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Build fingerprint
 BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
-BUILD_DESC := tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys
+BUILD_DESCRIPTION := tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
