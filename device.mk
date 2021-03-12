4bdbc8ae0e0ad2b221ab1fd155e95edf4b29de87
ItsVixano <vixanonew@gmail.com>
2021-03-12 23:29:59 +0100

    ysl: Merge overlays
    Change-Id: I3cdb47bb0f0f56198d1bc66602055faab5b4fca7

--- device.mk
+++ device.mk
@@ -22,7 +22,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
